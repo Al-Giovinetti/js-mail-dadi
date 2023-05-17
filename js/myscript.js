92a4aa18e955e21852e1f94081a4e36fcdd16898
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-17 19:53:13 +0200

    Corretto finale dadi e cancella mail quando errata

--- js/myscript.js
+++ js/myscript.js
@@ -2,21 +2,21 @@
 let btnDie = document.querySelector("button.my_btn-die");
 
 let divResult = document.querySelector("div.my_result-die");
+let pResult = document.createElement("p");   
 
 // Creo un event listener
 btnDie.addEventListener("click",
     function(){
-        //Creo 2 variabili - 1 per il dado del computer e uno per quello dell' utente
 
+        pResult=0
+
+        //Creo 2 variabili - 1 per il dado del computer e uno per quello dell' utente
         let computerDie = parseInt(Math.random() * (6 - 1 + 1) + 1);
-        //console.log(computerDie + " Dadi Computer");
         document.getElementById("computer-die").innerHTML= parseInt(computerDie)
 
         let userDie = parseInt(Math.random() * (6 - 1 + 1) + 1);
-        //console.log(userDie + " Dadi utente");
         document.getElementById("user-die").innerHTML= parseInt(userDie)
 
-        let pResult = document.createElement("p");   
         // chi ha il numero più alto vince
          if ( computerDie > userDie){
             //console.log("Computer vince");
@@ -28,8 +28,7 @@ btnDie.addEventListener("click",
             //console.log("E' un pareggio riprova");
             pResult="E' un pareggio"
         }
-
-        divResult.append(pResult);
+        divResult.innerHTML=pResult;
     }
 );
 
@@ -43,12 +42,12 @@ const btnLogin = document.querySelector("button.my_btn-login")
 
 btnLogin.addEventListener("click",
     function(){
-
+        let inputMail
         let contatore=0
 
         for(index = 0; index < mailArrey.length;index++){
 
-            let inputMail = document.getElementById("email-ok");
+             inputMail = document.getElementById("email-ok");
 
             if(inputMail.value == mailArrey[index]){
                 mailAccepted = true
@@ -59,11 +58,10 @@ btnLogin.addEventListener("click",
         }
         if(contatore == 1 ){
             document.getElementById("mail-result").innerHTML="mail valida";
-            document.getElementById("mail-result").classList.add("text-success")
-            
-            
+            document.getElementById("mail-result").classList.add("text-success")  
         }else{
             document.getElementById("mail-result").innerHTML="mail non valida";
+            inputMail.value=""
             document.getElementById("mail-result").classList.add("text-danger")
 
         }
