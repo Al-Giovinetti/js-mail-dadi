dae5d4cb4eeaedbb7a2181764ce298b40117ef46
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-17 15:32:14 +0200

    Creato bottone e collegato a un EventListner

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,13 @@
+// Do una variabile al bottone per giocare
+let btnDie = document.querySelector("button.my_btn-die")
+
+// Creo un event listener
+btnDie.addEventListener("click",
+    function(){
+        alert("ciao")
+    })
+
+
 //Creo 2 variabili - 1 per il dado del computer e uno per quello dell' utente
 
 let computerDie = parseInt(Math.random() * (6 - 1 + 1) + 1)
