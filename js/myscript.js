66d90ce04e26ddacd7b0ed21ab06e4e754de19af
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-17 17:26:25 +0200

    Arrey con soluzione

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,6 @@
 let btnDie = document.querySelector("button.my_btn-die");
 
 let divResult = document.querySelector("div.my_result-die");
-let pResult = document.createElement("p");   
 
 // Creo un event listener
 btnDie.addEventListener("click",
@@ -17,6 +16,7 @@ btnDie.addEventListener("click",
         console.log(userDie + " Dadi utente");
         document.getElementById("user-die").innerHTML= parseInt(userDie)
 
+        let pResult = document.createElement("p");   
         // chi ha il numero più alto vince
          if ( computerDie > userDie){
             console.log("Computer vince");
@@ -28,9 +28,35 @@ btnDie.addEventListener("click",
             console.log("E' un pareggio riprova");
             pResult="E' un pareggio"
         }
+
         divResult.append(pResult);
     }
 );
+
+// Es Arrey
+
+let inputMail = prompt("Inserisci una mail e vedi se con questa puoi entrare");
+
+const mailArrey = ["ale","dino","gino","pino"]
+
+let mailAccepted
+
+for(index = 0; index < mailArrey.length;index++){
+    if(inputMail == mailArrey[index]){
+        mailAccepted = true
+    }else{
+        mailAccepted = false
+    }
+}
+
+if (mailAccepted == true){
+    document.getElementById("mail-result").innerHTML="mail valida"
+}else{
+    document.getElementById("mail-result").innerHTML="mail non valida"
+
+}
+
+
 
 
 
