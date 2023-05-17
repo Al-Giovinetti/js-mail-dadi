3cdeca74412110e5783c25c94868615237268fc4
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-17 16:30:14 +0200

    Stampa esercizio dadi

--- js/myscript.js
+++ js/myscript.js
@@ -1,27 +1,37 @@
 // Do una variabile al bottone per giocare
-let btnDie = document.querySelector("button.my_btn-die")
+let btnDie = document.querySelector("button.my_btn-die");
+
+let divResult = document.querySelector("div.my_result-die");
+let pResult = document.createElement("p");   
 
 // Creo un event listener
 btnDie.addEventListener("click",
     function(){
-        alert("ciao")
-    })
+        //Creo 2 variabili - 1 per il dado del computer e uno per quello dell' utente
+
+        let computerDie = parseInt(Math.random() * (6 - 1 + 1) + 1);
+        console.log(computerDie + " Dadi Computer");
+        document.getElementById("computer-die").innerHTML= parseInt(computerDie)
+
+        let userDie = parseInt(Math.random() * (6 - 1 + 1) + 1);
+        console.log(userDie + " Dadi utente");
+        document.getElementById("user-die").innerHTML= parseInt(userDie)
+
+        // chi ha il numero più alto vince
+         if ( computerDie > userDie){
+            console.log("Computer vince");
+            pResult="Spiacente, il computer ha vinto"
+        }else if( userDie > computerDie){
+            console.log ("Utente vince");
+            pResult="Hai Vinto"
+        }else{
+            console.log("E' un pareggio riprova");
+            pResult="E' un pareggio"
+        }
+        divResult.append(pResult);
+    }
+);
 
 
-//Creo 2 variabili - 1 per il dado del computer e uno per quello dell' utente
 
-let computerDie = parseInt(Math.random() * (6 - 1 + 1) + 1)
-    console.log(computerDie + " Dadi Computer")
 
-let userDie = parseInt(Math.random() * (6 - 1 + 1) + 1)
-    console.log(userDie + " Dadi utente")
-
-// chi ha il numero più alto vince
-
-if ( computerDie > userDie){
-    console.log("Computer vince")
-}else if( userDie > computerDie){
-    console.log ("Utente vince")
-}else{
-    console.log("E' un pareggio riprova")
-}
