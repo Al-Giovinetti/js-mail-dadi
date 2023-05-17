87f0cf59325d6740d9534b49270427afec8186a3
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-17 18:32:47 +0200

    Correzione parte mail con stampa

--- js/myscript.js
+++ js/myscript.js
@@ -9,23 +9,23 @@ btnDie.addEventListener("click",
         //Creo 2 variabili - 1 per il dado del computer e uno per quello dell' utente
 
         let computerDie = parseInt(Math.random() * (6 - 1 + 1) + 1);
-        console.log(computerDie + " Dadi Computer");
+        //console.log(computerDie + " Dadi Computer");
         document.getElementById("computer-die").innerHTML= parseInt(computerDie)
 
         let userDie = parseInt(Math.random() * (6 - 1 + 1) + 1);
-        console.log(userDie + " Dadi utente");
+        //console.log(userDie + " Dadi utente");
         document.getElementById("user-die").innerHTML= parseInt(userDie)
 
         let pResult = document.createElement("p");   
         // chi ha il numero più alto vince
          if ( computerDie > userDie){
-            console.log("Computer vince");
+            //console.log("Computer vince");
             pResult="Spiacente, il computer ha vinto"
         }else if( userDie > computerDie){
-            console.log ("Utente vince");
+            //console.log ("Utente vince");
             pResult="Hai Vinto"
         }else{
-            console.log("E' un pareggio riprova");
+            //console.log("E' un pareggio riprova");
             pResult="E' un pareggio"
         }
 
@@ -35,25 +35,37 @@ btnDie.addEventListener("click",
 
 // Es Arrey
 
-let inputMail = prompt("Inserisci una mail e vedi se con questa puoi entrare");
+const mailArrey = ["ale@gmail.com","dino@gmail.com","gino@gmail.com","pino@gmail.com"];
 
-const mailArrey = ["ale","dino","gino","pino"]
+let mailAccepted;
 
-let mailAccepted
+const btnLogin = document.querySelector("button.my_btn-login")
 
-for(index = 0; index < mailArrey.length;index++){
-    if(inputMail == mailArrey[index]){
-        mailAccepted = true
-    }else{
-        mailAccepted = false
+btnLogin.addEventListener("click",
+    function(){
+
+        let contatore=0
+
+        for(index = 0; index < mailArrey.length;index++){
+
+            let inputMail = document.getElementById("email-ok");
+
+            if(inputMail.value == mailArrey[index]){
+                mailAccepted = true
+                contatore=1
+            }else{
+                mailAccepted = false
+            }
+        }
+        if(contatore == 1 ){
+            document.getElementById("mail-result").innerHTML="mail valida";
+            
+        }else{
+            document.getElementById("mail-result").innerHTML="mail non valida";
+        }
     }
-}
+);    
 
-if (mailAccepted == true){
-    document.getElementById("mail-result").innerHTML="mail valida"
-}else{
-    document.getElementById("mail-result").innerHTML="mail non valida"
-}
 
 
 
